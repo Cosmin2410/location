3313a2c47700631fa233d129629748a965328096
Nae Cosmin <cosmin@192-168-0-102.rdsnet.ro>
2023-01-27 11:34:45 +0200

    deleted console

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
   useEffect(() => {
     if (location?.latitude && location?.longitude) {
       writeLocation();
-      console.log('thiss  location', location);
     }
   }, [location]);
 
