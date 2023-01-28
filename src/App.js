4bc56d27b9d9edf28743cad14ba0ce8173370a19
Nae Cosmin <cosmin@192-168-0-102.rdsnet.ro>
2023-01-28 21:32:33 +0200

    return loader if lat and long undefined

--- src/App.js
+++ src/App.js
@@ -53,6 +53,19 @@ function App() {
     setLoading(false);
   };
 
+  if (!location.latitude && !location.longitude) {
+    return (
+      <MoonLoader
+        color={'#ffffff'}
+        loading={loading}
+        cssOverride={{}}
+        size={50}
+        aria-label='Loading Spinner'
+        data-testid='loader'
+      />
+    );
+  }
+
   return (
     <div className='App'>
       <header className='App-header'>
@@ -61,39 +74,35 @@ function App() {
             display: 'flex',
             flexDirection: 'column',
             height: '500px',
-            justifyContent: 'space-between',
+            justifyContent: 'center',
             position: 'relative',
+            alignItems: 'center',
           }}
         >
-          {location.latitude && location.longitude ? (
-            <>
-              {meme && !loading ? (
-                <img
-                  src={meme.url}
-                  alt={meme.name}
-                  style={{
-                    width: '300px',
-                    maxHeight: '400px',
-                    borderRadius: '5px',
-                  }}
-                />
-              ) : (
-                <MoonLoader
-                  color={'#ffffff'}
-                  loading={loading}
-                  cssOverride={{}}
-                  size={50}
-                  aria-label='Loading Spinner'
-                  data-testid='loader'
-                />
-              )}
-            </>
+          {meme && !loading ? (
+            <img
+              src={meme.url}
+              alt={meme.name}
+              style={{
+                width: '300px',
+                maxHeight: '400px',
+                borderRadius: '5px',
+              }}
+            />
           ) : (
-            <p>Accept location</p>
+            <MoonLoader
+              color={'#ffffff'}
+              loading={loading}
+              cssOverride={{}}
+              size={50}
+              aria-label='Loading Spinner'
+              data-testid='loader'
+            />
           )}
           <button
             onClick={handleClick}
             className={'generate-button'}
+            style={{ marginTop: 'auto' }}
           >
             Generate Meme
           </button>
